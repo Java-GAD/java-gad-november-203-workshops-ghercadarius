14d209401893d245338514d1ea45689a9e981284
Gherca Darius <ghercadarius@gmail.com>
2023-12-07 21:32:54 +0200

    half_triangle

--- prj1/src/Main.java
+++ prj1/src/Main.java
@@ -18,9 +18,21 @@ public class Main {
             System.out.print("*");
         }
     }
+
+    public static void printLeftHalfTriangle(int nr_rows){
+        for(int i = 1; i <= nr_rows; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nr_rows = sc.nextInt(), nr_columns = sc.nextInt();
         printHallowPattern(nr_rows, nr_columns);
+        System.out.print("\n");
+        printLeftHalfTriangle(nr_rows);
     }
 }
